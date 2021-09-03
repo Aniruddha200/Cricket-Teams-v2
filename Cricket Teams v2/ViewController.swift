a28c7c926008787c399f2053a32b4db656a51666
Aniruddha <aniruddha200baidya@protonmail.com>
2021-09-03 06:14:23 +0100

    Initial Commit

---
+++ Cricket Teams v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cricket Teams v2
+//
+//  Created by administrator on 03/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
